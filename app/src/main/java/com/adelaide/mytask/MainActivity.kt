568bef843ef5cc592393ac879fe0c71a31deafa9
AdelaideOtoobeaQuaye <https://github.com/QuayeAdelaideOtoobea>
2025-04-29 01:53:08 +0000

    progress

--- app/src/main/java/com/adelaide/mytask/MainActivity.kt
+++ app/src/main/java/com/adelaide/mytask/MainActivity.kt
@@ -7,37 +7,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import android.R.attr.letterSpacing
-import androidx.compose.ui.text.TextLayoutResult
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.graphics.Shadow
+import androidx.compose.material3.TextField
 import androidx.compose.ui.tooling.preview.Preview
 import com.adelaide.mytask.ui.theme.MyTaskTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             MyTaskTheme {
                 Surface {
-                    SayHello()
+                    Surface(color = MaterialTheme.colorScheme.background) {
+
+                    }
+
                 }
             }
         }
@@ -47,42 +46,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SayHello() {
+fun MyTextFieldUI() {
+    var text by remember { mutableStateOf("") }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Hello Dev",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            color = Color.Green,
-            fontSize = 40.sp,
-            fontStyle = FontStyle.Italic,
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily.SansSerif,
-            letterSpacing = 1.5.sp,
-            textDecoration = TextDecoration.Underline,
-            textAlign = TextAlign.Center,
-            lineHeight = 24.sp,
-            overflow = TextOverflow.Ellipsis,
-            softWrap = true,
-            maxLines = 1,
-            onTextLayout = { textLayoutResult: TextLayoutResult -> }
-
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = {Text("Enter your text")},
+            modifier = Modifier.fillMaxWidth()
         )
-    }
-
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MaterialTheme {
-        SayHello()
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "You entered; $text")
     }
 }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewMyTextFieldUI() {
+        MyTaskTheme {
+            MyTextFieldUI()
+        }
+    }
+
+
