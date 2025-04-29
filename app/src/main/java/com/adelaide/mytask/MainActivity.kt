44b591d0e4b7efcb45e9661db0b5007723775455
AdelaideOtoobeaQuaye <https://github.com/QuayeAdelaideOtoobea>
2025-04-29 17:26:06 +0000

    feedback form progress

--- app/src/main/java/com/adelaide/mytask/MainActivity.kt
+++ app/src/main/java/com/adelaide/mytask/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.adelaide.mytask
 
+import android.R.attr.checked
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.graphics.Color
@@ -23,13 +22,34 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import android.R.attr.letterSpacing
+import android.R.attr.onClick
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Switch
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.adelaide.mytask.ui.theme.MyTaskTheme
+import java.time.format.TextStyle
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,45 +57,126 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyTaskTheme {
                 Surface {
-                    SayHello()
+                    FeedbackForm()
                 }
             }
         }
-
-
     }
 }
 
 @Composable
-fun SayHello() {
+fun FeedbackForm() {
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
     ) {
+        Image(painter = painterResource(R.drawable.dog), contentDescription = "")
+
         Text(
-            text = "Hello Dev",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            color = Color.Green,
+            text = "Feedback Form",
+
+            modifier = Modifier,
             fontSize = 40.sp,
-            fontStyle = FontStyle.Italic,
             fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily.SansSerif,
-            letterSpacing = 1.5.sp,
-            textDecoration = TextDecoration.Underline,
-            textAlign = TextAlign.Center,
-            lineHeight = 24.sp,
-            overflow = TextOverflow.Ellipsis,
-            softWrap = true,
-            maxLines = 1,
-            onTextLayout = { textLayoutResult: TextLayoutResult -> }
-
+            color = Color(0xFF6200EE),
+            textAlign = TextAlign.Center
         )
-    }
 
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Name") },
+            placeholder = { Text("Enter your name") }
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Your Feedback") },
+            placeholder = { Text("Enter your Feedback") }
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+
+        Row(
+            modifier = Modifier,
+        ) {
+
+            Text(
+                text = "Subscribe to newsletter"
+            )
+            Spacer(modifier = Modifier.width(30.dp))
+
+
+            Switch(
+                checked = true,
+                onCheckedChange = null
+            )
+
+        }
+
+        Text("Rating:")
+
+        Row() {
+            Row() {
+                RadioButton(
+                    selected = false,
+                    onClick = {}
+                )
+                Text("Poor")
+            }
+            Row() {
+                RadioButton(
+                    selected = false,
+                    onClick = {}
+                )
+                Text("Average")
+            }
+        }
+
+        Row() {
+            Row() {
+                RadioButton(
+                    selected = false,
+                    onClick = {}
+                )
+                Text("Good")
+            }
+
+            Row() {
+                RadioButton(
+                    selected = false,
+                    onClick = {}
+                )
+                Text("Excellent")
+            }
+
+        }
+
+
+        Row() {
+            Checkbox(
+                checked = true,
+                onCheckedChange = null
+            )
+            Text("I confirm my feedback is honest")
+        }
+
+
+        Button(
+            onClick = {}
+        ) {
+            Text("Submit")
+        }
+    }
 }
 
 
@@ -83,6 +184,8 @@ fun SayHello() {
 @Composable
 fun DefaultPreview() {
     MaterialTheme {
-        SayHello()
+        FeedbackForm()
     }
 }
+
+
