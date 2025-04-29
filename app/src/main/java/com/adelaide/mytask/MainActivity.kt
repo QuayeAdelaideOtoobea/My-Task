73f58ad771f96f0794109a7ed2b48441171b334f
AdelaideOtoobeaQuaye <https://github.com/QuayeAdelaideOtoobea>
2025-04-29 00:45:27 +0000

    second commit

--- app/src/main/java/com/adelaide/mytask/MainActivity.kt
+++ app/src/main/java/com/adelaide/mytask/MainActivity.kt
@@ -4,17 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import android.R.attr.end
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxWidth
+import android.R.attr.onClick
+import android.widget.Toast
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,67 +33,92 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import android.R.attr.letterSpacing
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.adelaide.mytask.ui.theme.MyTaskTheme
+import kotlinx.coroutines.sync.Mutex
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             MyTaskTheme {
-                Surface {
-                    SayHello()
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    MyButton()
                 }
             }
         }
-
-
     }
 }
 
-@Composable
-fun SayHello() {
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Hello Dev",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            color = Color.Green,
-            fontSize = 40.sp,
-            fontStyle = FontStyle.Italic,
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily.SansSerif,
-            letterSpacing = 1.5.sp,
-            textDecoration = TextDecoration.Underline,
-            textAlign = TextAlign.Center,
-            lineHeight = 24.sp,
-            overflow = TextOverflow.Ellipsis,
-            softWrap = true,
-            maxLines = 1,
-            onTextLayout = { textLayoutResult: TextLayoutResult -> }
-
-        )
-    }
-
-}
-
-
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    MaterialTheme {
-        SayHello()
+    MyTaskTheme {
+        MyButton()
     }
 }
+
+@Composable
+fun MyButton() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        val context = LocalContext.current
+
+        Button(
+            onClick = {
+                Toast.makeText(
+                    context,
+                    "Welcome to Geeks for Geeks",
+                    Toast.LENGTH_LONG
+                ).show()
+            },
+            modifier = Modifier.padding(16.dp),
+            enabled = true,
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.Green,
+                containerColor = Color.Black
+            ),
+            elevation =
+                ButtonDefaults.buttonElevation(defaultElevation = 10.dp),
+            border = BorderStroke(width = 2.dp, brush = SolidColor(Color.Green)),
+
+            contentPadding = PaddingValues(
+                start = 20.dp,
+                top = 2.dp,
+                end = 20.dp,
+                bottom = 12.dp
+            ),
+            interactionSource = remember {
+                MutableInteractionSource()
+            }
+        ) {
+            Text(
+                text = "Geeks for Geeks",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic,
+                fontFamily = FontFamily.Serif
+            )
+        }
+    }
+}
+
+
+
+
+
